cb2ba9543010e20a5c216887438e5d921726e0b0
Ingmar Steiner <steiner@coli.uni-saarland.de>
2015-05-13 17:45:40 +0200

    foo plugin depends on and applies bar plugin, ditto for tasks

--- buildSrc/gradle-foo-plugin/src/main/groovy/FooPlugin.groovy
+++ buildSrc/gradle-foo-plugin/src/main/groovy/FooPlugin.groovy
@@ -3,8 +3,12 @@ import org.gradle.api.Project
 
 class FooPlugin implements Plugin<Project> {
     void apply(Project project) {
-        project.task('foo') << {
-            println "Hello from the FooPlugin"
+        project.rootProject.subprojects.find { it.name == 'bar-project' }?.apply plugin: 'bar'
+        project.task('foo') {
+            dependsOn ':foo-project:bar-project:bar'
+            doLast {
+                println "Hello from the FooPlugin"
+            }
         }
     }
 }
